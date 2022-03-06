b92371e5f03b698af7f116862e94087151bca8dd
akrigline <a.krigline@gmail.com>
2022-03-05 21:38:34 -0500

    wip

--- scripts/concentration-5e.js
+++ scripts/concentration-5e.js
@@ -11,6 +11,24 @@ export class Concentration5e {
       console.log(this.MODULE_TITLE, '|', ...args);
     }
   }
+
+  static getFakeConcentrationEffect(item) {
+    const itemEffectDuration = getDurationFromItem(item);
+
+    return {
+      changes: [],
+      disabled: false,
+      duration: itemEffectDuration,
+      icon: `modules/${Concentration5e.MODULE_NAME}/images/concentrating.svg`,
+      label: `${game.i18n.localize('DND5E.Concentration')} - ${item.name}`,
+      origin: item.uuid,
+      flags: {
+        core: {
+          statusId: 'concentrating',
+        }
+      }
+    };
+  }
 }
 
 Hooks.on("ready", async () => {
@@ -35,21 +53,7 @@ Hooks.on('Item5e.roll', (item, _roll, _options, actor) => {
     return;
   }
 
-  const itemEffectDuration = getDurationFromItem(item);
-
-  const fakeConcentrationEffectData = {
-    changes: [],
-    disabled: false,
-    duration: itemEffectDuration,
-    icon: `modules/${Concentration5e.MODULE_NAME}/images/concentrating.svg`,
-    label: `${game.i18n.localize('DND5E.Concentration')} - ${item.name}`,
-    origin: item.uuid,
-    flags: {
-      core: {
-        statusId: 'concentrating',
-      }
-    }
-  };
+  const fakeConcentrationEffectData = Concentration5e.getFakeConcentrationEffect(item);
 
   actor.createEmbeddedDocuments('ActiveEffect', [fakeConcentrationEffectData]);
 });
@@ -59,93 +63,145 @@ Hooks.on('renderAbilityUseDialog', (app, [html]) => {
 
   // if the item isn't a spell or isn't a concentration spell
   if (app.item.type !== 'spell' || !app.item.data.data.components?.concentration) {
-  Concentration5e.log("not a spell or not concentration", {
-    spell: app.item.type !== 'spell',
-    concnetration: app.item.data.data.components?.concentration,
-  })
-  return;
+    Concentration5e.log("not a spell or not concentration", {
+      spell: app.item.type !== 'spell',
+      concnetration: app.item.data.data.components?.concentration,
+    })
+    return;
   }
 
   // if there's no parent or the parent has no effects
   if (!app.item.parent?.effects.size) {
-  Concentration5e.log("actor has no effects")
-  return;
+    Concentration5e.log("actor has no effects")
+    return;
   }
 
-  const concentrationEffects = app.item.parent.effects.filter((effect) => 
-    effect.data.flags?.core?.statusId === 'concentrating'
+  const concentrationEffects = app.item.parent.effects.filter((effect) =>
+    !effect.isSuppressed && effect.data.flags?.core?.statusId === 'concentrating'
   );
 
   if (!concentrationEffects.length) {
-  Concentration5e.log("not concentrating")
-  return;
+    Concentration5e.log("not concentrating")
+    return;
   }
 
-  const concentratingOn = concentrationEffects.map((effect)=> effect.sourceName).filterJoin(', ');
+  const concentratingOn = concentrationEffects.map((effect) => effect.sourceName).filterJoin(', ');
 
-  const node = document.createRange()
-  .createContextualFragment(`
-  <p class="notification warning">
-    ${game.i18n.format('concentration-5e.CONCENTRATION_WARNING', {spell: app.item.name})}
+  const node = document.createRange().createContextualFragment(`
+  <p class="notification info">
+    ${game.i18n.format('concentration-5e.CONCENTRATION_WARNING', { spell: app.item.name })}
     <br>
-    <small>${game.i18n.format('concentration-5e.CURRENTLY_CONCENTRATING', {concentratingOn})}</small>
+    <small>${game.i18n.format('concentration-5e.CURRENTLY_CONCENTRATING', { concentratingOn })}</small>
   </p>
   `);
-
 
   const notificationNode = html.querySelector('.notification');
   const formNode = html.querySelector('form');
 
   formNode.insertBefore(node, notificationNode);
-  app.setPosition({height: 'auto'});
+  app.setPosition({ height: 'auto' });
 });
 
 
-  /**
-   * Gets default duration values from the provided item.
-   * Assumes dnd5e data model, falls back to 1 round default.
-   */
-   function getDurationFromItem(item, passive) {
-
-    if (passive === true) {
-      return undefined;
-    }
-
-    if (!!item?.data.data.duration?.value) {
-
-      let duration = {};
-
-      switch (item.data.data.duration.units) {
-        case 'hour':
-          duration.seconds = item.data.data.duration?.value * 60 * 60;
-          break;
-        case 'minute':
-          duration.seconds = item.data.data.duration?.value * 60;
-          break;
-        case 'day':
-          duration.seconds = item.data.data.duration?.value * 60 * 60 * 24;
-          break;
-        case 'month':
-          duration.seconds = item.data.data.duration?.value * 60 * 60 * 24 * 28;
-          break;
-        case 'year':
-          duration.seconds = item.data.data.duration?.value * 60 * 60 * 24 * 365;
-          break;
-        case 'turn':
-          duration.turns = item.data.data.duration?.value;
-          break;
-        case 'round':
-          duration.rounds = item.data.data.duration?.value;
-          break;
-        default:
-          duration.rounds = 1;
-          break;
-      }
-
-      return duration
-    }
-
-    return {
-      rounds: 1
-    }
+Hooks.on('Actor5e.applyDamage', async (actor, totalDamageTaken) => {
+  // abort if user healed or took no damage
+  if (Math.sign(totalDamageTaken) > -1) {
+    return;
   }
+
+  const concentrationEffects = actor.effects.filter(
+    (effect) => !effect.isSuppressed && effect.data.flags?.core?.statusId === 'concentrating'
+  );
+
+  if (!concentrationEffects.length) {
+    return;
+  }
+
+  const halfDamageTaken = Math.floor(Math.abs(totalDamageTaken) / 2);
+  const saveDc = halfDamageTaken < 10 ? 10 : halfDamageTaken;
+
+  concentrationEffects.forEach((effect) => {
+    const fakeConcentrationItem = new Item.implementation({
+      name: effect.data.label,
+      img: `modules/${Concentration5e.MODULE_NAME}/images/concentrating.svg`,
+      type: 'consumable',
+      data: {
+        description: {
+          value: game.i18n.format(`${Concentration5e.MODULE_NAME}.CHECK_PROMPT`, {
+            name: actor.name,
+            spell: effect.sourceName,
+            damage: Math.abs(totalDamageTaken)
+          }),
+        },
+        actionType: 'save',
+        save: {
+          ability: 'con',
+          dc: saveDc,
+          scaling: 'flat',
+        },
+      }
+    }, {temporary: true, parent: actor});
+
+    // gets the chat button's save dc label
+    fakeConcentrationItem.getSaveDC();
+
+    // removes some noise from the chat footer
+    delete fakeConcentrationItem.data.data.equipped;
+
+    Concentration5e.log(fakeConcentrationItem)
+    
+    fakeConcentrationItem.roll({
+      configureDialog: false,
+      rollMode: CONST.DICE_ROLL_MODES.PUBLIC
+    });
+  });
+});
+
+/**
+ * Gets default duration values from the provided item.
+ * Assumes dnd5e data model, falls back to 1 round default.
+ */
+function getDurationFromItem(item, passive) {
+
+  if (passive === true) {
+    return undefined;
+  }
+
+  if (!!item?.data.data.duration?.value) {
+
+    let duration = {};
+
+    switch (item.data.data.duration.units) {
+      case 'hour':
+        duration.seconds = item.data.data.duration?.value * 60 * 60;
+        break;
+      case 'minute':
+        duration.seconds = item.data.data.duration?.value * 60;
+        break;
+      case 'day':
+        duration.seconds = item.data.data.duration?.value * 60 * 60 * 24;
+        break;
+      case 'month':
+        duration.seconds = item.data.data.duration?.value * 60 * 60 * 24 * 28;
+        break;
+      case 'year':
+        duration.seconds = item.data.data.duration?.value * 60 * 60 * 24 * 365;
+        break;
+      case 'turn':
+        duration.turns = item.data.data.duration?.value;
+        break;
+      case 'round':
+        duration.rounds = item.data.data.duration?.value;
+        break;
+      default:
+        duration.rounds = 1;
+        break;
+    }
+
+    return duration
+  }
+
+  return {
+    rounds: 1
+  }
+}
